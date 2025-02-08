b92b935c78eaa0ce306aaf95d6c86f7941b35811
Squirrel <squirrelhomebrew@gmail.com>
2025-02-08 15:35:33 -0800

    Create greet.ts

---
+++ Peak TS/greet.ts
@@ -0,0 +1,10 @@
+function greet(name: string) : string {
+    return `hai, ${name}!`;
+}
+
+console.log(greet("greg"));
+
+/* did I cook noma greg */
+/* how run ima compile dw*/
+
+/* yayayayayay*/
