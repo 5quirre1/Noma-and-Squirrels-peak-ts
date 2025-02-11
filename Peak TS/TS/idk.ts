bf6a07c4abcdf1f2e1c2a81b84628db89c84ccfc
Squirrel <squirrelhomebrew@gmail.com>
2025-02-11 11:44:52 -0800

    Create idk.ts

---
+++ Peak TS/TS/idk.ts
@@ -0,0 +1,16 @@
+class FreakybobTeam {
+  isDev?: boolean;
+  isGrahpics?: boolean;
+  isPromoter?: boolean;
+  name: string = "";
+  age: string = "";
+  role: string = "";
+  constructor(name: string = "", age: string = "", role: string = "", isDev?: boolean) {
+    this.name = name;
+    this.age = age;
+    this.role = role;
+    this.isDev = isDev;
+  }
+}
+
+//I finish later
